f0a895c027e7eddd1e258caacb5dea7250e5f127
Kozzztia <kozzztia@ukr.net>
2022-01-08 12:10:41 +0200

    slick

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,29 +1,38 @@
 let main = document.querySelector('.main');
 let div = document.querySelector('.div');
 let out = document.querySelector('.out');
-let text = document.querySelector('.monitor');
+let monitor = document.querySelector('.monitor');
 let keys = document.querySelectorAll('.key');
 let space = document.querySelector('.space')
 let run = function (max) {
   return Math.floor(Math.random() * max)
 }
 
-text.oninput = function (event) {
+monitor.oninput = function (event) {
   for (let i = 0; i < keys.length; i++) {
     if (keys[i].innerHTML == event.data) {
-      keys[i].style.boxShadow = `0 0 20px rgb(${run(255)},${run(255)},${run(255)})`;
+      keys[i].style.boxShadow = `0 0 15px rgb(${run(255)},${run(255)},${run(255)})`;
     } else {
       keys[i].style.boxShadow = 'none';
     }
   }
 }
 
-for (let i = 0; i < keys.length; i++) {
-  keys[i].addEventListener('click', function (event) {
-    keys.forEach(function (key) {
-      key.style.boxShadow = 'none';
-    })
-    text.value += keys[i].innerHTML;
-    keys[i].style.boxShadow = `0 0 20px rgb(${run(255)},${run(255)},${run(255)})`;
-  })
-}
+let start = 0;
+let end = 0;
+
+out.addEventListener('touchstart', (event) => {
+  start = event.touches[0].screenX;
+  if (start < end) {
+    monitor.style.backgroundColor = 'red';
+    monitor.innerHTML = 'left';
+  }
+})
+
+out.addEventListener('touchend', (event) => {
+  end = event.changedTouches[0].screenX;
+  if (start > end) {
+    monitor.style.backgroundColor = 'green';
+    monitor.innerHTML = 'right';
+  }
+})
